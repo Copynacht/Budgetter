2280bff6f0e6a9db7b1ea66fea24bb2ced1fa246
Copynacht <zxxp1341@gmail.com>
2025-06-01 04:04:16 +0900

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
 # ファイルコピー処理
-cp "$SCRIPT_DIR/docker/.env.example" "$SCRIPT_DIR/docker/.env"
-echo "✅ docker/.env.example → docker/.env"
+cp "$SCRIPT_DIR/docker/.env.example" "$SCRIPT_DIR/.env"
+echo "✅ docker/.env.example → .env"
 
-cp "$SCRIPT_DIR/docker/budgetter-api/.env.example" "$SCRIPT_DIR/docker/.env-budgetter-api"
-echo "✅ docker/budgetter-api/.env.example → docker/.env-budgetter-api"
+cp "$SCRIPT_DIR/docker/budgetter-api/.env.example" "$SCRIPT_DIR/docker/.env.budgetter-api"
+echo "✅ docker/budgetter-api/.env.example → .env.budgetter-api"
